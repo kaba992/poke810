35c8757255f4eaddc885ced604496c7ffe99e138
Corentin Deseine <deseinecorentin@gmail.com>
2022-04-30 17:33:48 +0200

    routes ajoutées à lapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
+
+  resources :pokemons, only: %i[index show new create] do
+    resources :bookings, only: %i[create]
+  end
+
+  resources :bookings, only: %i[update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
