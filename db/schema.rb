564db9b15fc91adc16ebf66160b2fcea15764000
Corentin Deseine <deseinecorentin@gmail.com>
2022-05-07 12:04:20 +0200

    image ajoutée  à la classe Pokemon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_05_181537) do
+ActiveRecord::Schema.define(version: 2022_05_07_095909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_05_05_181537) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "image_url"
     t.index ["user_id"], name: "index_pokemons_on_user_id"
   end
 
