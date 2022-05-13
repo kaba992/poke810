2ac8342627937619e34d4e2027153fe35ee4c4b1
Corentin Deseine <deseinecorentin@gmail.com>
2022-05-13 16:15:47 +0200

    dashboard card toute faite + route booking/show

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
 
-  resources :pokemons, only: %i[index show new create] do
+  resources :pokemons, only: %i[index show new create destroy] do
     resources :bookings, only: %i[create]
   end
 
-  resources :bookings, only: %i[update]
+  resources :bookings, only: %i[show update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
